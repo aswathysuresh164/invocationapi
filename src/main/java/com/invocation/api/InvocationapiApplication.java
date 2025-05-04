b12b9ad8ae3d7fc185bc116be2e65e8a32cbcb3a
aswathysuresh164 <aswathisuresh164@gmail.com>
2025-05-04 20:48:50 +0530

    first commit

---
+++ src/main/java/com/invocation/api/InvocationapiApplication.java
@@ -0,0 +1,13 @@
+package com.invocation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvocationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvocationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invocation/api/InvocationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invocation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvocationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
